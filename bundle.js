8208a217c3dca67d17bd77c715f59897dfb65ff4
Arián Jiménez Majidi <arianjm@gmail.com>
2021-12-01 19:53:17 +0100

    Use shapes and new hover data.

--- bundle.js
+++ bundle.js
@@ -1,5 +1,6 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const whiteFont = { font: { color: 'black' } };
+const redLine = { line: { color: 'red', width: 1 } };
+
 plot(pyComplexVcdJson);
 
 function plot({ endtime, signals, timescale }) {
@@ -8,11 +9,10 @@ function plot({ endtime, signals, timescale }) {
         height: numSignals * 50,
         hovermode: 'closest',
         hoverinfo: 'none',
-        margin: { t: 50, b: 5, l: 120 },
+        margin: { t: 50, b: 5, l: 200 },
         paper_bgcolor: 'white',
         plot_bgcolor: 'white',
         showlegend: false,
-        // spikedistance: -1,
         xaxis: {
             automargin: true,
             rangemode: 'nonnegative',
@@ -25,14 +25,16 @@ function plot({ endtime, signals, timescale }) {
             spikesnap: 'cursor',
             spikethickness: 2,
             tickfont: { color: 'black' },
-            title: { ...whiteFont, text: `Time (${timescale})` },
+            title: { text: `Time (${timescale})` },
         },
     };
     const fractionOfPlot = 1 / numSignals;
     const annotationTexts = [];
+    const allShapes = [];
 
-    const data = Object.values(signals).flatMap(({ nets: [ { name, size, type } ], tv }, index) => {
-        const graphName = `${type} ${name}`;
+    const data = Object.values(signals).flatMap(({ nets: [ { hier, name, size, type } ], tv }, index) => {
+        const module = hier.replace('testbench', '');
+        const graphName = `${module.startsWith('.') ? module.substr(1) : module} ${name}`.replace('testbench', '');
         annotationTexts.push(graphName);
 
         const sizeInt = parseInt(size, 10);
@@ -41,14 +43,21 @@ function plot({ endtime, signals, timescale }) {
         layout[`yaxis${yAxisNumber}`] = makeYAxisLayout({ fractionOfPlot, index });
 
         if (parseInt(size, 10) === 1) {
-            return makeBinarySignal({ endtime, fractionOfPlot, index, name: `${type} ${name}`, tv });
+            const binarySignal = makeBinarySignal({ endtime, fractionOfPlot, index, name: graphName, timescale, tv });
+
+            allShapes.push(...binarySignal.shapes);
+
+            return binarySignal.data;
         }
 
-        return makeVectorSignal({ endtime, fractionOfPlot, index, name: `${type} ${name}`, size, tv });
-    });
+        const vectorSignal = makeVectorSignal({ endtime, fractionOfPlot, index, name: graphName, size, timescale, tv });
+
+        allShapes.push(...vectorSignal.shapes);
+
+        return vectorSignal.data;
+    }).filter(data => data !== null);
 
     layout.annotations = annotationTexts.map((text, index) => ({
-        ...whiteFont,
         x: 0,
         xanchor: 'right',
         xref: 'paper',
@@ -58,151 +67,179 @@ function plot({ endtime, signals, timescale }) {
         showarrow: false,
         text,
     }));
+    layout.shapes = allShapes;
 
     Plotly.newPlot(document.getElementById('plot'), data, layout);
 }
 
-function makeVectorSignal({ endtime, fractionOfPlot, index, name, size, tv }) {
-    const annotations = [];
+function makeVectorSignal({ endtime, fractionOfPlot, index, name, size, timescale, tv }) {
+    const shapes = [];
     const [ text, textposition ] = [ [], [] ];
-    const [ x1, y1, x2, y2 ] = [ [], [], [], [] ];
-    const [ undefinedX, undefinedY ] = [ [], [] ];
-    let previousYStr = tv[0][1];
-    let y1Up = true;
+    const [ x, y ] = [ [], [] ];
+    let previousYStr = null;
+    const zoomFactor = (endtime - 0) / 100000;
+    const yaxis = `y${index + 1}`;
+
+    let lastXInt = null;
+    let shapeMiddle = null;
+    let openShape = null;
+    let closeShape = null;
 
     tv.forEach(([ xInt, yStr ], signalChangeIndex) => {
-        // If current state is undefined.
+        shapeMiddle = `L${xInt},0.5`;
+        openShape = `M${xInt + zoomFactor},0 ${shapeMiddle} L${xInt + zoomFactor},1`
+        closeShape = ` H${xInt - zoomFactor} ${shapeMiddle} L${xInt - zoomFactor},0 Z`;
+
+        // Current state is undefined.
         if (isNaN(yStr)) {
-            // If previous state wasn't undefined.
-            if (!isNaN(previousYStr) && (signalChangeIndex > 0)) {
-                const previousYInt = parseInt(previousYStr, 10);
-
-                x1.push(xInt);
-                x2.push(xInt);
-                y1.push(y1[y1.length - 1]);
-                y2.push(y2[y2.length - 1]);
-                text.push('');
-                textposition.push('center center');
-                undefinedX.push(null, xInt);
-                undefinedY.push(null, previousYInt);
-            }
-            undefinedX.push(xInt);
-            undefinedY.push(0.5);
-        }
-        // If current state is not undefined.
-        else {
-            if (signalChangeIndex > 0) {
-                // If previous state was undefined.
+            if (signalChangeIndex) {
+                // Previous state was undefined. Finish previous line.
                 if (isNaN(previousYStr)) {
-                    x1.push(null, xInt);
-                    x2.push(null, xInt);
-                    y1.push(null, 0.5);
-                    y2.push(null, 0.5);
-                    undefinedX.push(xInt);
-                    undefinedY.push(0.5);
-                    text.push('', '');
-                    textposition.push('center center', 'center center');
+                    shapes[shapes.length - 1].x1 = xInt;
                 }
+                // Previous state wasn't undefined. Close previous shape.
                 else {
-                    x1.push(xInt);
-                    x2.push(xInt);
-                    y1.push(y1Up ? 0 : 1);
-                    y2.push(y1Up ? 1 : 0);
-                    text.push('');
-                    textposition.push('center center');
+                    shapes[shapes.length - 1].path += closeShape;
                 }
             }
 
-            x1.push(xInt, xInt);
-            x2.push(xInt, xInt);
-            y1.push(0.5, y1Up ? 1 : 0);
-            y2.push(0.5, y1Up ? 0 : 1);
-            y1Up = !y1Up;
-
-            const numLeadingZeroes = size - yStr.length;
-
-            text.push(`${'0'.repeat(numLeadingZeroes)}${yStr}`, '');
-            textposition.push('right center', 'center center');
+            shapes.push({ ...redLine, x0: xInt, y0: 0.5, y1: 0.5, yref: yaxis });
         }
+        // Current state is not undefined.
+        else {
+            if (signalChangeIndex) {
+                // Previous state was undefined
+                if (isNaN(previousYStr)) {
+                    shapes[shapes.length - 1].x1 = xInt;
+                }
+                // Previous state wasn't undefined. Close previous shape.
+                else {
+                    shapes[shapes.length - 1].path += closeShape;
+                }
+            }
+
+            shapes.push({ line: { color: 'lightgreen', width: 1 }, path: openShape, type: 'path', yref: yaxis });
+        }
+
+        x.push(xInt);
+        y.push(0.5);
+
+        const numLeadingZeroes = size - yStr.length;
+        const leadingZeroes = isNaN(yStr) ? '' : '0'.repeat(numLeadingZeroes);
+
+        text.push(`${leadingZeroes}${isNaN(yStr) ? '' : yStr}`);
+        textposition.push('right center');
+
+        lastXInt = xInt;
         previousYStr = yStr;
     });
 
     // Stretch final result to the end of time.
     if (isNaN(previousYStr)) {
-        undefinedX.push(endtime);
-        y.push(0.5);
+        shapes[shapes.length - 1].x1 += lastXInt;
     }
     else {
-        x1.push(endtime);
-        x2.push(endtime);
-        y1.push(y1[y1.length - 1]);
-        y2.push(y2[y2.length - 1]);
+        shapes[shapes.length - 1].path += ` H${endtime - zoomFactor} L${endtime},0.5 L${endtime - zoomFactor},0 Z`;
     }
 
-    const common = { mode: 'lines', type: 'scattergl', yaxis: `y${index + 1}` };
-    const data = [
-        { ...common, line: { color: 'red', width: 1 }, name, x: undefinedX, y: undefinedY },
-        { ...common, line: { color: 'lightgreen', width: 1 }, mode: 'lines+text', name, text, textfont: { color: 'black' }, textposition, x: x1, y: y1 },
-        { ...common, line: { color: 'lightgreen', width: 1 }, name, x: x2, y: y2 },
-    ];
+    const hovertemplate = `${name} @ %{x:.0f} ${timescale.substr(1)}<br>%{text}`;
 
-    return data;
+    return {
+        data: {
+            hovertemplate,
+            mode: 'text',
+            name,
+            text,
+            textfont: { color: 'black' },
+            textposition,
+            type: 'scattergl',
+            x,
+            y,
+            yaxis,
+        },
+        shapes,
+    };
 }
 
-function makeBinarySignal({ endtime, index, name, tv }) {
+function makeBinarySignal({ endtime, index, name, timescale, tv }) {
+    const shapes = [];
+    const [ text, textposition ] = [ [], [] ];
     const [ x, y ] = [ [], [] ];
     const [ undefinedX, undefinedY ] = [ [], [] ];
-    let previousYStr = tv[0][1];
+    let previousYStr = null;
+    const yaxis = `y${index + 1}`;
+    const greenLine = { line: { color: 'green', width: 1 }, type: 'line' };
 
     tv.forEach(([ xInt, yStr ], signalChangeIndex) => {
-        if (isNaN(yStr)) {
-            if (!isNaN(previousYStr) && (signalChangeIndex > 0)) {
-                const previousYInt = parseInt(previousYStr, 2);
-                x.push(xInt);
-                y.push(previousYInt);
-                undefinedX.push(null, xInt);
-                undefinedY.push(null, previousYInt);
-            }
-            undefinedX.push(xInt);
-            undefinedY.push(0.5);
-        }
-        else {
-            if (signalChangeIndex > 0) {
-                if (isNaN(previousYStr)) {
-                    x.push(null, xInt);
-                    y.push(null, 0.5);
-                    undefinedX.push(xInt);
-                    undefinedY.push(0.5);
-                }
-                else {
-                    x.push(xInt);
-                    y.push(parseInt(previousYStr, 2));
-                }
-            }
+        const yInt = parseInt(yStr, 2)
+        const previousYInt = parseInt(previousYStr, 2);
 
-            x.push(xInt);
-            y.push(parseInt(yStr, 2));
+        // Current state is undefined
+        if (isNaN(yStr)) {
+            if (signalChangeIndex) {
+                // Previous state was undefined. Finish previous undefined line.
+                if (isNaN(previousYStr)) {
+                    shapes[shapes.length - 1].path += ` H ${xInt}`;
+                }
+                // Previous state was not undefined. Make a red line to the center of y-axis.
+                else {
+                    shapes.push({ ...redLine, path: `M ${xInt},${previousYStr} V 0.5`, type: 'path', yref: yaxis });
+                }
+            }
+            else {
+                shapes.push({ ...redLine, path: `M ${xInt},0.5`, type: 'path', yref: yaxis });
+            }
         }
+        // Current state is not undefined.
+        else {
+            if (signalChangeIndex) {
+                // Previous state was undefined. Make green line.
+                if (isNaN(previousYStr)) {
+                    shapes[shapes.length - 1].path += ` H ${xInt}`;
+                    shapes.push({ ...greenLine, path: `M ${xInt},0.5 V ${yStr}`, type: 'path', yref: yaxis })
+                }
+                // Previous state was not undefined. Continue green line.
+                else {
+                    shapes[shapes.length - 1].path += ` H ${xInt} V ${yInt}`;
+                }
+            }
+            else {
+                shapes.push({ ...greenLine, path: `M ${xInt},${yInt}`, type: 'path', yref: yaxis });
+            }
+        }
+
+        x.push(xInt);
+        y.push(0.5);
+
+        text.push(yStr);
+        textposition.push('right center');
+
+        lastXInt = xInt;
         previousYStr = yStr;
     });
 
+    shapes[shapes.length - 1].path += `H ${endtime}`;
     // Stretch final result to the end of time.
-    if (isNaN(previousYStr)) {
-        undefinedX.push(endtime);
-        y.push(0.5);
-    }
-    else {
-        x.push(endtime);
-        y.push(y[y.length - 1]);
-    }
+    x.push(endtime);
+    y.push(0.5);
 
-    const common = { mode: 'lines', type: 'scattergl', yaxis: `y${index + 1}` };
-    const data = [
-        { ...common, line: { color: 'red', width: 1 }, name, x: undefinedX, y: undefinedY },
-        { ...common, line: { color: 'green', width: 1 }, name, x, y },
-    ];
+    const hovertemplate = `${name} @ %{x:.0f} ${timescale.substr(1)}<br>%{text}`;
 
-    return data;
+    return {
+        data: {
+            hovertemplate,
+            mode: 'none',
+            name,
+            text,
+            textfont: { color: 'black' },
+            textposition,
+            type: 'scattergl',
+            x,
+            y,
+            yaxis,
+        },
+        shapes,
+    };
 }
 
 function makeYAxisLayout({ fractionOfPlot, index }) {
