735a88f9083de498b762d363d660bf03fe23581f
Arián Jiménez Majidi <arianjm@gmail.com>
2022-04-12 14:42:08 +0200

    Advance

--- player/player.js
+++ player/player.js
@@ -1,4 +1,5 @@
-const stage = new Konva.Stage({ container: 'konva', width: 600, height: 400 });
+const konvaWidth = 600;
+const stage = new Konva.Stage({ container: 'konva', width: konvaWidth, height: 400 });
 let layer = new Konva.Layer();
 let currentStep = 0;
 let currentInstruction = 0;
@@ -20,10 +21,16 @@ function getInstructionsForNextStep() {
     return animationInstructions.slice(currentInstruction, animationInstructions.findIndex((instr, index) => (index > currentInstruction) && (instr.type === 'step')))
 }
 
-// Needed for builder, not player
+function scrollToBottom() {
+    output.scrollTop = output.scrollHeight;
+}
+
+// needed for builder, not player
 function goToInstruction(instructionNumber) {
     const undoOrRedo = (instructionNumber - currentInstruction) < 0 ? 'undo' : 'redo';
     let instructionStepChange = undoOrRedo === 'redo' ? 1 : -1;
+
+    debugger;
 
     while (currentInstruction !== instructionNumber) {
         let instruction = animationInstructions[currentInstruction];
@@ -45,6 +52,7 @@ function goToInstruction(instructionNumber) {
             }
         }
     }
+    scrollToBottom();
 }
 
 function goToStep(stepNumber) {
@@ -69,11 +77,15 @@ function goToEnd() {
 }
 
 function stopAnimating() {
+    animationInstructions.forEach(instruction => {
+        instruction.animate?.pause();
+    });
     console.log('TODO: Stop animating');
     // Stop any animating. Note: The promises need to resolve with forcedStop
 }
 
 async function playNextStep() {
+    document.getElementById('play').disabled = true;
     instructionsInStep = getInstructionsForNextStep();
 
     while (instructionsInStep.length) {
@@ -100,6 +112,7 @@ async function playNextStep() {
         currentInstruction++;
         currentStep++;
     }
+    document.getElementById('play').disabled = false;
 }
 
 // Actions
@@ -117,15 +130,22 @@ function backToStartPressed() {
 function playButtonPressed() {
     // TODO:
     // if on last step
-    if (currentInstruction >= animationInstructions.length) {
+    if (currentInstruction >= animationInstructions.length - 1) {
         goToBeginning();
     }
     playNextStep();
+    scrollToBottom();
 }
 
 (function() {
     const [ height, width ] = [ 50, 50 ];
     const objects = [
+        new Konva.Text({
+            align: 'center',
+            fontFamily: 'HelveticaNeue-Light,"Helvetica Neue Light","Helvetica Neue",Helvetica,Arial,"Lucida Grande",sans-serif',
+            fontSize: 15,
+            width: konvaWidth,
+        }),
         new Konva.Rect({
             fill: 'blue',
             height,
@@ -152,44 +172,31 @@ function playButtonPressed() {
     layer.add(...objects);
     stage.add(layer);
 
-    const duration = .5;
     animationInstructions = [
         { caption: 'Move Rect 1, then make bigger', type: 'step' },
         {
-            animate: new Konva.Tween({
-                duration,
-                easing: Konva.Easings.EaseInOut,
-                node: layer.find(`#0`)[0],
-                x: 200,
-                y: 150
-            }),
-            objId: '0',
+            nextState: { x: 200, y: 150 },
+            objId: '1',
             previousState: { x: 100, y: 100 },
         },
         {
-            animate: new Konva.Tween({
-                duration,
-                easing: Konva.Easings.EaseInOut,
-                node: layer.find(`#0`)[0],
-                height: 100,
-                width: 100
-            }), // { height: 80, width: 80 },
-            objId: '0',
+            nextState: { height: 100, width: 100 },
+            objId: '1',
             previousState: { height: 50, width: 50 },
         },
         { caption: 'Make Circle 1 bigger and move above Rect 1 at the same time', type: 'step' },
-        // {
-        //     animate: { radius: 80 },
-        //     objId: '1',
-        //     previousState: { radius: 10 },
-        // },
-        // {
-        //     delay: 0,
-        //     animate: {x: 500, y: 250 },
-        //     objId: '1',
-        //     previousState: { x: 50, y: 50 },
-        // },
-        // { caption: 'Animation end', type: 'step' },
+        {
+            nextState: { radius: 80 },
+            objId: '2',
+            previousState: { radius: 10 },
+        },
+        {
+            nextState: { x: 500, y: 250 },
+            objId: '2',
+            previousState: { x: 50, y: 50 },
+            delay: 0,
+        },
+        { caption: 'Animation end', type: 'step' },
     ];
 
     animationInstructions.forEach((instruction, index) => {
@@ -204,6 +211,14 @@ function playButtonPressed() {
             });
             document.getElementById('timeline').appendChild(newButton);
         }
+        else {
+            instruction.animate = new Konva.Tween({
+                duration: 1,
+                easing: Konva.Easings.EaseInOut,
+                node: layer.find(`#${instruction.objId}`)[0],
+                ...instruction.nextState,
+            });
+        }
         animationSteps[animationSteps.length - 1].instructions.push(instruction);
 
         const output = document.getElementById('output');
@@ -215,9 +230,9 @@ function playButtonPressed() {
             }
             else {
                 instruction.animate.onFinish = () => { resolve(); };
-                debugger;
+                instruction.animate.reset();
                 instruction.animate.play();
-                output.appendChild(document.createTextNode(`  Playing instruction index ${index}: ${JSON.stringify(instruction.animate)}\n`));
+                output.appendChild(document.createTextNode(`  Playing instruction index ${index}: ${JSON.stringify(instruction.nextState)}\n`));
             }
         });
         instruction.undo = () => {
@@ -225,11 +240,8 @@ function playButtonPressed() {
                 output.appendChild(document.createTextNode(`Undoing step ${currentStep}, instruction index ${index}: ${instruction.caption}\n`));
             }
             else {
+                instruction.animate.reset();
                 output.appendChild(document.createTextNode(`  Undoing instruction index ${index}: ${JSON.stringify(instruction.previousState)}\n`));
-                layer.find(`#${instruction.objId}`)[0].to({
-                    ...instruction.previousState,
-                    duration: 0,
-                });
             }
         };
         instruction.redo = () => {
@@ -237,26 +249,24 @@ function playButtonPressed() {
                 output.appendChild(document.createTextNode(`Redoing step ${currentStep}, instruction index ${index}: ${instruction.caption}\n`));
             }
             else {
-                layer.find(`#${instruction.objId}`)[0].to({
-                    ...instruction.animate,
-                    duration: 0,
-                });
-                output.appendChild(document.createTextNode(`  Redoing instruction index ${index}: ${JSON.stringify(instruction.animate)}\n`));
+                instruction.animate.finish();
+                output.appendChild(document.createTextNode(`  Redoing instruction index ${index}: ${JSON.stringify(instruction.nextState)}\n`));
             }
         };
     });
 
-    currentStep = animationSteps.length;
-    currentInstruction = animationInstructions.length;
+    goToEnd();
 
     const undoButton = document.getElementById('undo');
 
     undoButton.addEventListener('click', function() {
         if (currentInstruction <= 0) {
             output.appendChild(document.createTextNode(`-- Already at the beginning, can't go back.\n`));
+            scrollToBottom();
             return;
         }
         goToInstruction(currentInstruction - 1);
+        scrollToBottom();
     });
 
     document.getElementById('play').addEventListener('click', playButtonPressed);
@@ -264,8 +274,10 @@ function playButtonPressed() {
     document.getElementById('redo').addEventListener('click', function() {
         if (currentInstruction >= animationInstructions.length) {
             output.appendChild(document.createTextNode(`-- Already at the end, can't go forward.\n`));
+            scrollToBottom();
             return;
         }
         goToInstruction(currentInstruction + 1);
+        scrollToBottom();
     });
 })();
